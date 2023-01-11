132a3680470fa56ec58d9844a93724dbe89db4ce
Glydewell Burdick <glydewellb@gmail.com>
2023-01-10 18:26:01 -0600

    layout information section

---
+++ src/components/EmailButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function EmailButton() {
+    return (
+        <div className="information">
+            <h1>Information</h1>
+            <p>Information</p>
+        </div>
+    )
+}
--- src/components/Information.js
+++ src/components/Information.js
@@ -1,10 +1,19 @@
 import React from "react";
+import EmailButton from "./EmailButton";
+import LinkedInButton from "./LinkedInButton";
 
 export default function Information() {
     return (
         <div className="information">
-            <h1>Information</h1>
-            <p>Information</p>
+            <h1 className="dev-name">Glydewell Burdick</h1>
+            <h5 className="dev-title">Full Stack Web Developer</h5>
+            <p className="dev-site"><a href="https://www.glyde.dev">glyde.dev</a></p>
+            <EmailButton />
+            <LinkedInButton />
+            <h3>About</h3>
+            <p></p>
+            <h3>Interests</h3>
+            <p></p>
         </div>
     )
 }

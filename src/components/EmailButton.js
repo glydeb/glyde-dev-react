f56398ecd3e4ade8d834d76e060cdb2e96d151d1
Glydewell Burdick <glydewellb@gmail.com>
2023-02-18 10:34:36 -0600

    start work on buttons

--- src/components/EmailButton.js
+++ src/components/EmailButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function EmailButton() {
     return (
-        <div className="information">
+        <div className="email-button">
             <h1>Information</h1>
             <p>Information</p>
         </div>
--- src/components/LinkedInButton.js
+++ src/components/LinkedInButton.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function LinkedInButton() {
     return (
-        <div className="information">
-            <h1>Information</h1>
-            <p>Information</p>
+        <div className="linkedin-button">
+            <button>
+                <a href="https://www.linkedin.com/in/glydewell-burdick-a9260b114/">
+                    LinkedIn <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                </a>
+            </button>
         </div>
     )
 }
